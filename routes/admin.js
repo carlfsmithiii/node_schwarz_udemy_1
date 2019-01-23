4b52b07860e305e6fee0a04bcf4944b5f6d5f2dd
Carl Smith <carlfsmithiii@gmail.com>
2019-01-23 12:59:02 -1000

    convert to handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,10 @@ router.get("/add-product", (req, res, next) => {
   // res.sendFile(path.join(rootDir, "views", "add-product.html"));
   res.render("add-product", {
     pageTitle: "Add Product",
-    path: "/admin/add-product"
+    path: "/admin/add-product",
+    formsCSS: true,
+    productsCSS: true,
+    activeAddProduct: true
   });
 });
 
